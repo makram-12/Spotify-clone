b2d0f611f3825b9407cbfa398257fa203c67ad62
makram-12 <ma4501146@gmail.com>
2026-01-21 18:30:16 +0200

    Initial commit for full project

---
+++ spotify-admin/src/pages/AddSong.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useState } from 'react'
+import axios from 'axios'
+import { url } from '../App'
+import { toast } from 'react-toastify'
+import { useEffect } from 'react'
+const AddSong = () => {
+
+    const [image, setImage] = useState(false);
+    const [song, setSong] = useState(false);
+    const [name, setName] = useState("");
+    const [desc, setDesc] = useState("");
+    const [album, setAlbum] = useState("none");
+    const [loading, setLoading] = useState(false);
+    const [albumData, setAlbumData] = useState([]);
+
+    const onSubmitHandler = async (e) => {
+
+        e.preventDefault();
+        setLoading(true)
+        try {
+
+            const formData = new FormData();
+
+            formData.append('name', name);
+            formData.append('desc', desc);
+            formData.append('image', image);
+            formData.append('audio', song);
+            formData.append('album', album);
+
+            const response = await axios.post(`${url}/api/song/add`, formData);
+
+            if (response.data.success) {
+                toast.success("Song added");
+                setName("");
+                setDesc("");
+                setAlbum("none");
+                setImage(false);
+                setSong(false);
+            } else {
+                toast.error("Something went wrong")
+            }
+
+            // eslint-disable-next-line no-unused-vars
+        } catch (error) {
+            toast.error("Error occured");
+        }
+        setLoading(false);
+    }
+
+    const loadAlbumData = async () => {
+        try{
+            const response = await axios.get(`${url}/api/album/list`);
+
+            if(response.data.success){
+                setAlbumData(response.data.albums);
+            }else{
+                toast.error("Unable to load albums data");
+            }
+        // eslint-disable-next-line no-unused-vars
+        }catch(error){
+            toast.error("Error occur");
+        }
+    }
+
+    useEffect(()=>{
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        loadAlbumData();
+    },[])
+
+    return loading ? (
+        <div className='grid place-items-center min-h-[80vh]'>
+            <div className='w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin'>
+
+            </div>
+        </div>
+    ) : (
+        <form onSubmit={onSubmitHandler} className='flex flex-col items-start gap-8 text-gray-600'>
+            <div className='flex gap-8'>
+                <div className='flex flex-col gap-4'>
+                    <p>Upload Song</p>
+                    <input onChange={(e) => setSong(e.target.files[0])} type="file" id='song' accept='audio/*' hidden />
+                    <label htmlFor="song">
+                        <img src={song ? assets.upload_added : assets.upload_song} className='w-24 cursor-pointer' alt="" />
+                    </label>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <p>Upload Image</p>
+                    <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' accept='image/*' hidden />
+                    <label htmlFor="image">
+                        <img src={image ? URL.createObjectURL(image) : assets.upload_area} className='w-24 cursor-pointer' alt="" />
+                    </label>
+                </div>
+            </div>
+            <div className='flex flex-col gap-2.5'>
+                <p>Song name</p>
+                <input onChange={(e) => setName(e.target.value)} value={name} type="text" className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' placeholder='Tyoe Here' required />
+            </div>
+
+            <div className='flex flex-col gap-2.5'>
+                <p>Song description</p>
+                <input onChange={(e) => setDesc(e.target.value)} value={desc} type="text" className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]' placeholder='Tyoe Here' required />
+            </div>
+
+            <div className='flex flex-col gap-2.5'>
+                <p>Album</p>
+                <select onChange={(e) => setAlbum(e.target.value)} defaultValue={album} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-37.5'>
+                    <option value="none">None</option>
+                    {albumData.map((item,index)=>(<option key={index} value={item.name}>{item.name}</option>))}
+                </select>
+            </div>
+
+            <button type='submit' className='text-base bg-black text-white py-2.5 px-14 cursor-pointer'>ADD</button>
+
+        </form>
+    )
+}
+
+export default AddSong
---
+++ spotify-backend/src/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connnectDB = async () => {
+
+    mongoose.connection.on('connected',() => {
+        console.log("connection established");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
+
+}
+
+export default connnectDB;
